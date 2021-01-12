59daa7070dfb8cc79008f747d45f8226c72d6329
akshay1027 <akshayar1027@gmail.com>
2021-01-12 23:51:45 +0530

    Create rest.py

---
+++ REST/rest.py
@@ -0,0 +1,15 @@
+import requests
+
+# Set the API endpoint
+
+url = "https://www.metaweather.com/api/location/search/?query=san"
+
+
+# Use the library to perform an HTTP GET request to the URL
+
+response = requests.get(url)
+
+
+# Print out the data
+
+print(response.text)
