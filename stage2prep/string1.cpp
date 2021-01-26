02fd1cdb7b036fb296cc770d89f269ea34e14cfe
akshay1027 <akshayar1027@gmail.com>
2021-01-26 19:04:51 +0530

    Create string1.cpp

---
+++ stage2prep/string1.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+
+char ques[3]="abc";
+char que[4];
+
+    cin.getline(que, 4); 
+
+    cout<<"a"<<endl;
+    cout<<"ab"<<endl;
+    cout<<"abc"<<endl;
+    cout<<"abcd"<<endl;
+    cout<<"abd"<<endl;
+    cout<<"ac"<<endl;
+    cout<<"acd"<<endl;
+    cout<<"ad"<<endl;
+    cout<<"b"<<endl;
+    cout<<"bc"<<endl;
+    cout<<"bcd"<<endl;
+    cout<<"bd"<<endl;
+    cout<<"c"<<endl;
+    cout<<"cd"<<endl;
+    cout<<"d"<endl;
+
+        return 0;
+}
