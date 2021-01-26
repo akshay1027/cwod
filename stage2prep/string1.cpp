bae441f7191f312eec8ffe6bd1c281ed2c2827c9
akshay1027 <akshayar1027@gmail.com>
2021-01-26 19:16:35 +0530

    Update string1.cpp

--- stage2prep/string1.cpp
+++ stage2prep/string1.cpp
@@ -1,33 +1,41 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
-
-
-int main() {
-
-char ques[3]="abc";
-char que[4];
-
-    cin.getline(que, 4); 
-
-    cout<<"a"<<endl;
-    cout<<"ab"<<endl;
-    cout<<"abc"<<endl;
-    cout<<"abcd"<<endl;
-    cout<<"abd"<<endl;
-    cout<<"ac"<<endl;
-    cout<<"acd"<<endl;
-    cout<<"ad"<<endl;
-    cout<<"b"<<endl;
-    cout<<"bc"<<endl;
-    cout<<"bcd"<<endl;
-    cout<<"bd"<<endl;
-    cout<<"c"<<endl;
-    cout<<"cd"<<endl;
-    cout<<"d"<endl;
-
-        return 0;
+#define AAA                \
+  ios::sync_with_stdio(0); \
+  cin.tie(0);              \
+  cout.tie(0);
+#define endl "\n"
+#define ll long long
+#define ld long double
+#define SQ(a) (a) * (a)
+#define F first
+#define S second
+int32_t main()
+{
+  AAA;
+  string s;
+  cin >> s;
+  int n = s.length();
+  int k = pow(2, n);
+  sort(s.begin(), s.end());
+  vector<string> ans;
+  for (int i = 1; i < k; i++)
+  {
+    string t;
+    for (int j = 0; j < n; j++)
+    {
+      int a = (int)pow(2, j) & i;
+      if (a)
+      {
+        // cout << pow(2, j) << " " << i << " " << (pow(2, j) & i) << endl;
+        t.push_back(s[j]);
+      }
+    }
+    ans.push_back(t);
+  }
+  sort(ans.begin(), ans.end());
+  for (int i = 0; i < ans.size(); i++)
+  {
+    cout << ans[i] << endl;
+  }
 }
